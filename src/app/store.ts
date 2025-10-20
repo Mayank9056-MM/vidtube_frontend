199f20f913e565c534504bde0dc2e6e8cb66e3eb
Mayank Mahajan <mayankmahajan9056@gmail.com>
2025-10-20 23:21:04 +0530

    update store for user and bug fix in hook.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from '@/features/user/userSlice'
-import videoReducer from '@/features/video/videoSlice'
+// import videoReducer from '@/features/video/videoSlice'
 
 
 export const store = configureStore({
-reducer: { user: userReducer, video: videoReducer },
+reducer: { user: userReducer,
+    //  video: videoReducer 
+    },
 devTools: import.meta.env.MODE !== 'production',
 })
 
