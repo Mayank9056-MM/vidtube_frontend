3bd927d47a17824c627d4c1cdbd7bdc2b0bcfce3
Mayank Mahajan <mayankmahajan9056@gmail.com>
2025-11-16 19:30:34 +0530

    feat(auth): add PublicRoute to block login/register for authenticated users
    docs: added JSDoc for useAuth hook and route components for better maintainability

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -11,6 +11,14 @@ import type { JSX } from "react";
 import Loader from "@/components/common/Loader";
 import { logger } from "@/utls/logger";
 
+/**
+ * PrivateRoute is a higher-order component that wraps a route
+ * and checks if the user is authenticated before rendering the route.
+ * If the user is not authenticated, it redirects to the login page.
+ * If the user is authenticated, it renders the route.
+ * @param {{ children: JSX.Element }} props
+ * @returns {JSX.Element} The rendered component
+ */
 const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   const { isAuthenticated, initialized, loading } = useAuth();
 
@@ -24,12 +32,53 @@ const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   return isAuthenticated ? children : <Navigate to="/login" replace />;
 };
 
+/**
+ * PublicRoute is a higher-order component that wraps a route
+ * and checks if the user is authenticated before rendering the route.
+ * If the user is authenticated, it redirects to the home page.
+ * If the user is not authenticated, it renders the route.
+ * @param {{ children: JSX.Element }} props
+ * @returns {JSX.Element} The rendered component
+ */
+const PublicRoute = ({ children }: { children: JSX.Element }) => {
+  const { isAuthenticated, initialized, loading } = useAuth();
+
+  if (!initialized || loading) {
+    return <Loader />;
+  }
+
+  // If logged in → don't allow login/register → redirect to home
+  return isAuthenticated ? <Navigate to="/" replace /> : children;
+};
+
+/**
+ * AppRoutes component renders the routes for the application.
+ * It contains public routes that are restricted for logged-in users
+ * and protected routes that are only accessible for logged-in users.
+ * The routes are wrapped with either PublicRoute or PrivateRoute components
+ * to handle the authentication logic.
+ */
 export const AppRoutes = () => {
   return (
     <Routes>
-      {/* Public Routes */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      {/* Public routes but restricted for logged-in users */}
+      <Route
+        path="/login"
+        element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        }
+      />
+
+      <Route
+        path="/register"
+        element={
+          <PublicRoute>
+            <Register />
+          </PublicRoute>
+        }
+      />
 
       {/* Protected Routes */}
       <Route
